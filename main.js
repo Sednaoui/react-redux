3409c778109ef5ec5998d3dac37a52b7ad3b0dc1
sednaoui <marcsednaoui@hotmail.com>
2019-09-29 11:28:49 +0200

    close buttom to new cards

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// close buttom and append to each card
+let myNodelist = document.getElementsByTagName("li");
+for (let  i= 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
 //add new card
 function newTask() {
     event.preventDefault();
@@ -14,16 +24,18 @@ function newTask() {
       }
     
     //make add task input to zero after submit
-    document.getElementById("addNewTaskInput").value = "";
-}
-
-// close buttom and append to each card
-let myNodelist = document.getElementsByTagName("li");
-for (let  i= 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+    // document.getElementById("addNewTaskInput").value = "";
+    let spanClose = document.createElement("SPAN");
+    let txtClose = document.createTextNode("\u00D7");
+    spanClose.className = "close";
+    spanClose.appendChild(txtClose);
+    task.appendChild(spanClose);
+    
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
 }
 

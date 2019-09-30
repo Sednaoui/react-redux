cbdd077a6496eb5cb893aaf288bf07431358713d
sednaoui <marcsednaoui@hotmail.com>
2019-09-30 13:53:49 +0200

    add scroll to list

--- twello-reactJS/src/List.js
+++ twello-reactJS/src/List.js
@@ -2,20 +2,26 @@ import React from "react";
 import "./List.css";
 
 class List extends React.Component {
-    render() {
-        return (
-            <div class="myMainContainer">
-                <div class="myContainer myList">
-                    <h3> List1</h3>
-                    <ul id="myListOfTasks">
-                        <li class="mycard">task1</li>
-                        <li class="mycard">task2</li>
-                        <li class="mycard">task3</li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="myMainContainer">
+        <div class="myContainer myList">
+          <h3> List1</h3>
+          <ul id="myListOfTasks" class="scrollList">
+            <li class="mycard">task1</li>
+            <li class="mycard">task2</li>
+            <li class="mycard">task3</li>
+            <li class="mycard">task3</li>
+            <li class="mycard">task3</li>
+            <li class="mycard">task3</li>
+            <li class="mycard">task3</li>
+            <li class="mycard">task3</li>
+            <li class="mycard">task5</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default List;

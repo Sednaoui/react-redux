e5ab7b2d4a884bef0a80c35d5d42769f16b470af
sednaoui <marcsednaoui@hotmail.com>
2019-10-04 13:16:02 +0200

    use form the react way

--- twello-reactJS/src/List.js
+++ twello-reactJS/src/List.js
@@ -7,32 +7,42 @@ import { connect } from "react-redux";
 import { addTask } from "./Actions/actionCreator";
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.value.trim()) {
+      return;
+    }
+    store.dispatch(addTask(this.state.value));
+    this.setState({ value: ""})
+  }
 
   render() {
-    let input;
     return (
       <div className="myMainContainer">
         <div className="myContainer myList">
           <h3>{this.props.title}</h3>
-          {this.props.cardReducer.map((eachDescription,i) => (
-            <Card {...eachDescription} key={eachDescription.id} index={i}/>
+          {this.props.cardReducer.map((eachDescription, i) => (
+            <Card {...eachDescription} key={eachDescription.id} index={i} />
           ))}
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              console.log(this.input);
-              if (!this.input.value.trim()) {
-                return;
-              }
-              store.dispatch(addTask(this.input.value));
-              this.input.value = "";
-            }}
-          >
-            <input ref={node => (this.input = node)} />
-            <button
-              type="submit">
-              Add Card
-            </button>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Add Card</button>
           </form>
         </div>
       </div>
@@ -48,7 +58,7 @@ List.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  cardReducer: state.taskReducer,
+  cardReducer: state.taskReducer
 });
 
 export default connect(mapStateToProps)(List);

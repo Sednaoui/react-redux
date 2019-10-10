c57841fd40dccbffbd839f7fdd1099237902e572
sednaoui <marcsednaoui@hotmail.com>
2019-10-10 15:07:59 +0200

    addLis case for activity

--- twello-reactJS/src/Reducers/activityReducer.js
+++ twello-reactJS/src/Reducers/activityReducer.js
@@ -6,6 +6,11 @@ const activityReducer = (state = initial_Activity, action) => {
       let newState = state;
       newState.push(`You created a new Card: ` + newCardAdded);
       return newState;
+    case "ADD_LIST":
+      const newListAdded = action.payload.title;
+      let newStateList = state;
+      newStateList.push(`You created a new List: ` + newListAdded);
+      return newStateList;
     default:
       return state;
   }

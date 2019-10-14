cbc26bbabb8d62f398fc95e8284c359ddaf47230
sednaoui <marcsednaoui@hotmail.com>
2019-10-14 13:53:55 +0200

    use const instead of let for single use variables

--- twello-reactJS/src/Reducers/activityReducer.js
+++ twello-reactJS/src/Reducers/activityReducer.js
@@ -3,26 +3,26 @@ const activityReducer = (state = initialActivity, action) => {
     switch (action.type) {
     case 'ADD_TASK':
         const newCardAdded = action.payload.text;
-        let newState = state;
+        const newState = state;
         newState.push(`You created a new Card: ` + newCardAdded);
         return newState;
     case 'ADD_LIST':
         const newListAdded = action.payload.title;
-        let newStateList = state;
+        const newStateList = state;
         newStateList.push(`You created a new List: ` + newListAdded);
         return newStateList;
     case 'EDIT_CARD':
         const editCard = action.value;
-        let newStateEditCard = state;
+        const newStateEditCard = state;
         newStateEditCard.push(`You edited your card name to: ` + editCard);
         return newStateEditCard;
     case 'EDIT_LIST':
         const editList = action.value;
-        let newStateEditList = state;
+        const newStateEditList = state;
         newStateEditList.push(`You edited your list name to: ` + editList);
         return newStateEditList;
     case 'CLOSE_TASK':
-        let newStateCloseTask = state;
+        const newStateCloseTask = state;
         newStateCloseTask.push(`You closed your card`);
         return newStateCloseTask;
     default:

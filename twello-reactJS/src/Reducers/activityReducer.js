1b023352e340c3d6656baf911bc2552e5e900a7a
sednaoui <marcsednaoui@hotmail.com>
2019-10-14 13:56:18 +0200

    strings single quote in activityReducer

--- twello-reactJS/src/Reducers/activityReducer.js
+++ twello-reactJS/src/Reducers/activityReducer.js
@@ -4,26 +4,26 @@ const activityReducer = (state = initialActivity, action) => {
     case 'ADD_TASK':
         const newCardAdded = action.payload.text;
         const newState = state;
-        newState.push(`You created a new Card: ` + newCardAdded);
+        newState.push('You created a new Card: ' + newCardAdded);
         return newState;
     case 'ADD_LIST':
         const newListAdded = action.payload.title;
         const newStateList = state;
-        newStateList.push(`You created a new List: ` + newListAdded);
+        newStateList.push('You created a new List: ' + newListAdded);
         return newStateList;
     case 'EDIT_CARD':
         const editCard = action.value;
         const newStateEditCard = state;
-        newStateEditCard.push(`You edited your card name to: ` + editCard);
+        newStateEditCard.push('You edited your card name to: ' + editCard);
         return newStateEditCard;
     case 'EDIT_LIST':
         const editList = action.value;
         const newStateEditList = state;
-        newStateEditList.push(`You edited your list name to: ` + editList);
+        newStateEditList.push('You edited your list name to: ' + editList);
         return newStateEditList;
     case 'CLOSE_TASK':
         const newStateCloseTask = state;
-        newStateCloseTask.push(`You closed your card`);
+        newStateCloseTask.push('You closed your card');
         return newStateCloseTask;
     default:
         return state;

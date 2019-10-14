d6e6467fae39c5627a7882c02a2d293bad2f2ea9
sednaoui <marcsednaoui@hotmail.com>
2019-10-14 14:29:14 +0200

    template literals instead of string concatenation

--- twello-reactJS/src/Reducers/activityReducer.js
+++ twello-reactJS/src/Reducers/activityReducer.js
@@ -3,20 +3,21 @@ const activityReducer = (state = initialActivity, action) => {
     const newState = state;
     const editCard = action.value;
     const editList = action.value;
+    const newCardAdded = action.text;
+    const newListAdded = action.title;
+
     switch (action.type) {
     case 'ADD_TASK':
-        const newCardAdded = action.payload.text;
-        newState.push('You created a new Card: ' + newCardAdded);
+        newState.push(`You created a new Card: ${newCardAdded}`);
         return newState;
     case 'ADD_LIST':
-        const newListAdded = action.payload.title;
-        newState.push('You created a new List: ' + newListAdded);
+        newState.push(`You created a new List: ${newListAdded}`);
         return newState;
     case 'EDIT_CARD':
-        newState.push('You edited your card name to: ' + editCard);
+        newState.push(`You edited your card name to: ${editCard}`);
         return newState;
     case 'EDIT_LIST':
-        newState.push('You edited your list name to: ' + editList);
+        newState.push(`You edited your list name to: ${editList}`);
         return newState;
     case 'CLOSE_TASK':
         newState.push('You closed your card');

4bdcc29f76ac80f7cc7a416df2b2120540c7096c
sednaoui <marcsednaoui@hotmail.com>
2019-10-17 11:57:37 +0200

    create constants in a seperate file

--- twello-reactJS/src/Actions/Constants.js
+++
@@ -1,8 +0,0 @@
-// constants for action types lists
-export const ADD_LIST = 'ADD_LIST';
-export const EDIT_LIST = 'EDIT_LIST';
-
-// constants for actions types cards
-export const ADD_CARD = 'ADD_CARD';
-export const CLOSE_CARD = 'CLOSE_CARD';
-export const EDIT_CARD = 'EDIT_CARD';
---
+++ twello-reactJS/src/app/cardComponent/Constants.js
@@ -0,0 +1,5 @@
+export default {
+    ADD_CARD: 'ADD_CARD',
+    CLOSE_CARD: 'CLOSE_CARD',
+    EDIT_CARD: 'EDIT_CARD',
+};

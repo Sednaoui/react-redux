c57355ff4b9ee9ed49f4bd638e339d280086df96
sednaoui <marcsednaoui@hotmail.com>
2019-10-17 11:57:06 +0200

    listActionCreators restructure

---
+++ twello-reactJS/src/app/listComponent/listActionCreator.js
@@ -0,0 +1,13 @@
+import shortid from 'shortid';
+import Constants from './Constants';
+
+export const addList = (title) => ({
+    type: Constants.ADD_LIST,
+    payload: { title, listID: shortid.generate(), cards: [] },
+});
+
+export const editList = (listID, value) => ({
+    type: Constants.EDIT_LIST,
+    payload: { listID,
+        value },
+});

b23433ee228cfbfa930e81bf2ac34feb592c8506
sednaoui <marcsednaoui@hotmail.com>
2019-10-17 11:55:02 +0200

    make initial state objects instead of arrays

---
+++ twello-reactJS/src/app/Reducers/initialConstants.js
@@ -0,0 +1,40 @@
+import shortid from 'shortid';
+
+const listId1 = shortid.generate();
+const listId2 = shortid.generate();
+const cardId1 = shortid.generate();
+const cardId2 = shortid.generate();
+
+export const initialActivity = {
+    activityList: ['Welcome to new Twello Board'],
+};
+
+export const listInitialState = {
+    lists: {
+        [listId1]: {
+            listID: listId1,
+            title: 'List Name 1',
+            cards: [cardId1],
+        },
+        [listId2]: {
+            listID: listId2,
+            title: 'List Name 2',
+            cards: [cardId2],
+        },
+    },
+};
+
+export const cardInitialState = {
+    cards: {
+        [cardId1]: {
+            cardID: cardId1,
+            text: 'Task 3',
+            listID: listId1,
+        },
+        [cardId2]: {
+            cardID: cardId2,
+            text: 'Task 4',
+            listID: listId2,
+        },
+    },
+};
